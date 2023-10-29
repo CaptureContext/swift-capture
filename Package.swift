30152ccbf6e412a388f2754d3df6b77a6a008da8
maximkrouk <id.maximkrouk@gmail.com>
2023-10-29 17:42:03 +0100

    fix: Use .git protocol extension for docc plugin url

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
   ],
   dependencies: [
     .package(
-      url: "https://github.com/apple/swift-docc-plugin",
+      url: "https://github.com/apple/swift-docc-plugin.git",
       from: "1.3.0"
     ),
   ],
